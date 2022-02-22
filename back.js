9a516a433946a9495beab84786e02d658350608f
alex <bez-al@mail.ru>
2022-02-22 23:22:28 +0300

    22.02.22

--- back.js
+++ back.js
@@ -1,1 +1,26 @@
-var poll = requer()
+const fs = require('fs');
+
+fs.readdir('./logs/users/', (err, files) => {
+    if(err) throw err; // не прочитать содержимое папки
+    console.log('В папке находятся файлы:' + files);
+    files.forEach(item=>{
+        fs.readFile('./logs/users/' + item, (err, data) => {
+            if (err) throw err;
+            let users = JSON.parse(data);
+            console.log('user: ', item)
+            console.log(users);
+        })
+    })
+ });
+ fs.readFile('./logs/pool/pool.status','utf8',(err, data) => {
+    if (err) throw err;
+
+    let re = /{([\s\S]+?)}/g
+    let str = data.match(re)
+    console.log(str)
+    let arrjsn = [];
+    str.forEach(item=>{
+        arrjsn.push(JSON.parse(item))
+    })
+    console.log(arrjsn)
+})
