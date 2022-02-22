aeb1547f2bda676195df3c0aad2b52b8d6ef44df
alex <bez-al@mail.ru>
2022-02-23 00:05:38 +0300

    22.02.22

--- back.js
+++ back.js
@@ -1,26 +1,42 @@
-const fs = require('fs');
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
+app.use(express.static('dist'));
+app.listen(8080, ()=>{
+	console.log('Back run  on 8080 port');
+});
 
-fs.readdir('./logs/users/', (err, files) => {
-    if(err) throw err; // не прочитать содержимое папки
-    console.log('В папке находятся файлы:' + files);
-    files.forEach(item=>{
-        fs.readFile('./logs/users/' + item, (err, data) => {
-            if (err) throw err;
-            let users = JSON.parse(data);
-            console.log('user: ', item)
-            console.log(users);
+app.get('/users', function(reg, res, next){
+    fs.readdir('./logs/users/', (err, files) => {
+        if(err) throw err; // не прочитать содержимое папки
+        console.log('В папке находятся файлы:' + files);
+        console.log('send users: ', files);
+        res.end(JSON.stringify(files))
+    });
+});
+
+app.get('/user1', function(reg, res, next){
+    fs.readFile('./logs/users/dgb1q9a8tp75sw0mgzpe2555g889azpx66er4gnr9e8', (err, data) => {
+        if (err) throw err;
+        let user = JSON.parse(data);
+        console.log('send user1', user);
+        res.end(JSON.stringify(user))
+    })
+});
+
+app.get('/pool', function(reg, res, next){
+    fs.readFile('./logs/pool/pool.status','utf8',(err, data) => {
+        if (err) throw err;
+        let re = /{([\s\S]+?)}/g
+        let str = data.match(re)
+        let arrjsn = [];
+        str.forEach(item=>{
+            arrjsn.push(JSON.parse(item))
         })
+        console.log('send pool:', arrjsn)
+        res.end(JSON.stringify(arrjsn))
     })
- });
- fs.readFile('./logs/pool/pool.status','utf8',(err, data) => {
-    if (err) throw err;
-
-    let re = /{([\s\S]+?)}/g
-    let str = data.match(re)
-    console.log(str)
-    let arrjsn = [];
-    str.forEach(item=>{
-        arrjsn.push(JSON.parse(item))
-    })
-    console.log(arrjsn)
-})
+});
